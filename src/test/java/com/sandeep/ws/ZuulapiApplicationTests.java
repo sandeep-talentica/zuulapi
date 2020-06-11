60039f5124275dc86feccde84302e195e952d8ac
Sandeep <dhalosandeepthakur@gmail.com>
2020-06-11 12:28:48 +0530

    First Commit

---
+++ src/test/java/com/sandeep/ws/ZuulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
